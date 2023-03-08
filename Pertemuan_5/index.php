5c01cad5bc7e6f498c3d8531c1e453de8ea283e6
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-03-08 09:14:24 +0700

    [ADD] sample database

--- Pertemuan_5/index.php
+++ Pertemuan_5/index.php
@@ -15,14 +15,16 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Employees</h1>
-    <table border=1>
+    <h1 align="center">Employees</h1>
+    <table border=1 align="center">
         <thead>
             <tr>
-                <td>No.</td>
+                <td>Employee Number</td>
                 <td>Full Name</td>
+                <td>Extension</td>
                 <td>Email</td>
                 <td>Office Code</td>
+                <td>Reports To</td>
                 <td>Job Title</td>
             </tr>
         </thead>
@@ -31,19 +33,23 @@
             <tr>
                 <td><?= $data["employeeNumber"]; ?></td>
                 <td><?= $data["firstName"]." ".$data["lastName"]; ?></td>
+                <td><?= $data["extension"]; ?></td>
                 <td><?= $data["email"]; ?></td>
                 <td><?= $data["officeCode"]; ?></td>
+                <td><?= $data["reportsTo"]; ?></td>
                 <td><?= $data["jobTitle"]; ?></td>
             </tr>
         <?php endwhile ?>
         </tbody>
     </table>
-    <h1>Product Lines</h1>
-    <table border=1>
+    <h1 align="center">Product Lines</h1>
+    <table border=1 align="center">
         <thead>
             <tr>
                 <td>Product Line</td>
                 <td>Text Description</td>
+                <td>HTML Description</td>
+                <td>Image</td>
             </tr>
         </thead>
         <tbody>
@@ -51,6 +57,8 @@
             <tr>
                 <td><?= $data["productLine"] ?></td>
                 <td><?= $data["textDescription"] ?></td>
+                <td><?= $data["htmlDescription"] ?></td>
+                <td><?= $data["image"] ?></td>
             </tr>
         <?php endwhile ?>
         </tbody>
